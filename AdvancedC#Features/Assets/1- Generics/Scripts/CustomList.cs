9c4d7c586ef350bcb9aa6ce13c6c782b48d3477f
darcy <darcy.astill@gmail.com>
2017-07-24 16:04:50 +1000

    GenericsTest

---
+++ AdvancedC#Features/Assets/1- Generics/Scripts/CustomList.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Generics
+{
+    public class CustomList<T>
+    {
+        public T[] list;
+
+        public int amount
+        {
+            get;
+            private set;
+        }
+
+        public CustomList() {amount = 0;}
+        public T this[int index]
+        {
+            get
+            {
+                return list[index];
+            }
+            set
+            {
+                list[index] = value;
+            }
+        }
+
+        public void Add(T item)
+        {
+            //Create a new arrayof ammount + 1
+            T[] cache = new T[amount + 1];
+            //check if the list has been initialized
+            if (list != null)
+            {
+                //copy all exitsting items to new array
+                for (int i = 0; i < list.Length; i++)
+                {
+                    cache[i] = list[i];
+                }
+            }
+
+            //place new item at end index
+            cache[amount] = item;
+
+            //replace old array wth new array
+            list = cache;
+            //increment
+            amount++;
+        }
+    }
+
+}
