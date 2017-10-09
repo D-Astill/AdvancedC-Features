e0e5e210aff5406ab3abfca9ac88c3638bd15b57
d-astill <darcy.astill@gmail.com>
2017-10-09 15:42:49 +1100

    Networking

---
+++ Assets/5 - Networking/Scripts/LocalUser.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Networking
+{
+    [RequireComponent(typeof(Player))]
+    public class LocalUser : MonoBehaviour
+    {
+        private Player player;
+
+        void Start()
+        {
+            player = GetComponent<Player>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            float h = Input.GetAxis("Mouse X");
+            h += Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+            player.Move(h, v);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                player.Jump();
+            }
+        }
+    }
+
+}
+
---
+++ Assets/5 - Networking/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Networking
+{
+    public class Player : MonoBehaviour
+    {
+        public float movementSpeed = 8f;
+        public float rotationSpeed = 360f;
+        public float jumpHeight = 2.0f;
+
+        private bool isGrounded = false;
+        private Rigidbody rb;
+        void Awake()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        public void Move(float h, float v)
+        {
+            Vector3 pos = rb.position;
+            Quaternion rot = rb.rotation;
+
+            pos += transform.forward * v * movementSpeed * Time.deltaTime;
+            rot *= Quaternion.AngleAxis(rotationSpeed * h * Time.deltaTime, Vector3.up);
+
+            rb.MovePosition(pos);
+            rb.MoveRotation(rot);
+
+        }
+
+        public void Jump()
+        {
+            if (isGrounded)
+            {
+                rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+                isGrounded = false;
+            }
+        }
+
+        void OnCollisionEnter(Collision other)
+        {
+            isGrounded = true;
+        }
+    }
+
+}
+
